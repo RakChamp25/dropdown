3bfd60cf89402c2dec074b27177865cf9dbc4b6e
RakChamp25 <ajay25kools@gmail.com>
2019-01-21 10:24:21 +0530

    dropdown focus

--- app/components/dropdown-component.js
+++ app/components/dropdown-component.js
@@ -1,43 +1,52 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    
     headVal: null,
     dropdownBool : false ,
     listObj : null ,
     filterVal : null ,
     listData : null ,
+    // mouseLeave() {
+    //     this.send("hideDrop");
+    // },
     actions:{
         showDrop(){
             this.set('dropdownBool',true);
         },
         hideDrop(){
             this.set('dropdownBool',false);
+            this.set('filterVal','');
         },
-        setArray( ListDropdown ){
-            this.send("showDrop");
+        focusInput(){
+            document.getElementById('filterDrop').focus();
+        },
+        async setArray( ListDropdown ){
+            await this.send("showDrop");
             this.set('listObj',ListDropdown);
             this.set('listData',ListDropdown);
-            //this.$('#filterDrop').focus();    
+             this.send("focusInput");   
         },
         setHeadVal(list){
             this.set('headVal',list);
             this.send("hideDrop");
         },
-        filterByValue(){
-           let dummy = [];
-           for(let i=0 ; i < this.get('listObj').length ; i++ )
-           {
-                if( this.listObj[i].name.toLowerCase().includes(this.get('filterVal') )){
-                    dummy.push(this.get('listObj')[i]);
-                } 
-           }
+        filterByValue(){ 
+            
            if( this.get('filterVal')==='' ){
              this.set('listObj',this.get('listData'));
            }
            else{
+            
+            let dummy = [];
+            for(let i=0 ; i < this.get('listData').length ; i++ )
+            {
+                 if( this.listObj[i].name.toLowerCase().includes(this.get('filterVal').toLowerCase() )){
+                     dummy.push(this.get('listObj')[i]);
+                 } 
+            }
             this.set('listObj',dummy);
            }
-            
         }
 
     }
